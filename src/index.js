c15df0eb1977220e42f8f6c31beeaec1ec00cb9a
Rafael <rafaeldacunhatheodoro@gmail.com>
2021-08-05 17:52:03 -0300

    HTML + JavaScript

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const fodao ='Rafa' 
+const element =<h1>O Pica das galáxias é o:{fodao}</h1>
+ReactDOM.render(element ,document.getElementById('root')
 );
 
