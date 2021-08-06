8c2ce4748af33c171e710c78838effaaacc3ecfe
Rafael <rafaeldacunhatheodoro@gmail.com>
2021-08-06 16:52:17 -0300

    Chamando funçao

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const fodao ='Rafa' 
-const element =<h1>O Pica das galáxias é o:{fodao}</h1>
+function formatarNome(usuarios){
+    return usuarios.firsName + ' ' + usuarios.lastName + '- idade ' + usuarios.idade;
+}
+
+const usuarios={
+    firsName: 'Rafao',
+    lastName: 'React',
+    idade: '32'
+};
+
+const element = (<h1>Hello, {formatarNome(usuarios)}</h1>)
+
 ReactDOM.render(element ,document.getElementById('root')
 );
 
