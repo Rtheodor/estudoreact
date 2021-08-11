7f8fca10218579e3a7d2914b65e99bea9adc8749
Rafael <rafaeldacunhatheodoro@gmail.com>
2021-08-10 22:43:28 -0300

    Tentativas e erros

--- src/index.js
+++ src/index.js
@@ -4,14 +4,24 @@ import ReactDOM from 'react-dom';
 /*function Welcome(props){
     return <h1>Hello, {props.name}</h1>;
 }*/
-class Welcome extends React.Component{
+/*class Welcome extends React.Component{
     render(){
         return <h1>Hello, {this.props.name}</h1>
+                
+    }
+}*/
+
+class Programador extends React.Component{
+    render(){
+        return <h1>Hello, {this.props.mundo}</h1>
+                
     }
 }
-const element = <Welcome name="Rafao"/>;
+/*
+const element = <Welcome name="Rafao"/>;*/
+const teste = <Programador mundo="Aprendendo a ser um programador"/>
 
-ReactDOM.render(element ,document.getElementById('root'));
+ReactDOM.render(teste ,document.getElementById('root'));
 
 
 
