c43c9e6e3f6aabeafca009c420f4400f797807f0
Rafael <rafaeldacunhatheodoro@gmail.com>
2021-08-11 23:24:37 -0300

    Usando components no React

--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-/*function Welcome(props){
-    return <h1>Hello, {props.name}</h1>;
-}*/
-/*class Welcome extends React.Component{
-    render(){
-        return <h1>Hello, {this.props.name}</h1>
-                
-    }
-}*/
 
-class Programador extends React.Component{
-    render(){
-        return <h1>Hello, {this.props.mundo}</h1>
-                
-    }
-}
-/*
-const element = <Welcome name="Rafao"/>;*/
-const teste = <Programador mundo="Aprendendo a ser um programador"/>
-
-ReactDOM.render(teste ,document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 
