dfc3cd73c6a495e6fd37d02eabdd5b76a6256949
Rafael <rafaeldacunhatheodoro@gmail.com>
2021-08-16 10:20:10 -0300

    Form React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React from 'react';
 
 class App extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {nome: '', email: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleChangeEmail = this.handleChangeEmail.bind(this);
+        this.handSubmit = this.handSubmit.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({nome: event.target.value});
+    }
+
+    handleChangeEmail (event){
+        this.setState({email: event.target.value});
+    }
+    
+    handSubmit(event){
+        alert('Olá,' + this.state.nome + ' seu email é ' + this.state.email);
+        event.preventDefault();
+    }
+
+    render(){
         return (
-            <NumberList numbers = {numbers}/>
+            <div>
+                <p>Salve {this.state.nome}, o seu email é {this.state.email}</p>
+            <form onSubmit={this.handSubmit}>
+                <label> Nome </label>
+                <input type="text" name="nome" onChange={this.handleChange} /><br />< br />
+                <label> Email </label>
+                <input type="text" name="email" onChange={this.handleChangeEmail} /><br />
+                <input type="submit" value="Enviar"/>
+            </form>
+            </div>
         );
     }
 }
-
-function NumberList(props){
-    const numbers = props.numbers;
-    const listItens = numbers.map((number)=>
-        <li key={number.toString()}>{number}</li>
-    );
-    return(
-        <ul>{listItens}</ul>
-    )
-}
-
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 export default App;
