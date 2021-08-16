ac7624e17c0b3604364198270e6fc5e405242423
Rafael <rafaeldacunhatheodoro@gmail.com>
2021-08-16 08:22:20 -0300

    Renderizaçao condicionar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { botao: true };
-
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick() {
-        this.setState(state => ({
-            botao: !state.botao
-        }));
-    }
-
     render() {
         return (
-            <button onClick={this.handleClick}>
-                {this.state.botao ? 'ON' : 'OFF'}
-            </button>
+            <Greeting isLoggedIn={false} />
         )
     }
+}
+
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if(isLoggedIn){
+        return <UserGreeting />
+    }
+    return <GuestGreeting />
+}
+
+function UserGreeting(props) {
+    return <h1>Welcome</h1>
+
+}
+
+function GuestGreeting(props){
+    return <h1>Please sign up.</h1>
 }
 
 export default App;
